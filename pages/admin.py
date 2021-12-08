53d3110a3649a4dd3ba60c3a151fd7c2bd18c896
Ganesh Patil <ganeshpatil.1997@gmail.com>
2021-12-08 01:52:01 +0000

    Initial Commit

---
+++ pages/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from pages.models import Team
+
+# Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src= "{}" width = "40" style="border-radius: 50px;"/>'.format(object.photo.url))
+
+
+    thumbnail.short_description = 'Photo'
+    list_display = ('id','thumbnail','first_name','designation','created_date')
+    list_display_links = ('first_name','id')
+    search_fields = ('first_name', 'last_name','designation')
+
+
+
+admin.site.register(Team,TeamAdmin)
